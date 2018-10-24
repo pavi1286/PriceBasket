39665fd5fc1b735a2eb6d14f1b62c678326ff1a5
Pavithra Jayarajan <pavithra.jayarajan@gmail.com>
2018-10-24 10:58:16 +0100

    Adding Discount class

---
+++ Discounts/IDiscount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discounts
+{
+    public interface IDiscount
+    {
+        void AddDiscounts();
+        int CalculateDiscounts();
+    }
+}
--- PriceBasket/Program.cs
+++ PriceBasket/Program.cs
@@ -8,25 +8,30 @@ namespace PriceBasket
 {
     class Program
     {
-        static List<Goods> lGoods = new List<Goods>();
-        static List<String> lCartItems = new List<String>();
-        static Receipt FinalReceipt = new Receipt();
+        static List<Goods> _goodsList = new List<Goods>();
+        static List<String> _cartItems = new List<String>();
+        static Receipt _finalReceipt = new Receipt();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the items");
-            lCartItems = Console.ReadLine().Split(' ').ToList();
+            Console.WriteLine("Enter the list of items");
+
+            _cartItems = Console.ReadLine().Split(' ').ToList();
 
 
-            
-            lGoods.Add(new Goods { ItemName = "Soup", Price = .65, UnitofMeasure = "tin" });
-            lGoods.Add(new Goods { ItemName = "Bread", Price = .80, UnitofMeasure = "loaf" });
-            lGoods.Add(new Goods { ItemName = "Milk", Price = 1.3, UnitofMeasure = "bottle" });
-            lGoods.Add(new Goods { ItemName = "Apples", Price = 1, UnitofMeasure = "bag" });
+
+            _goodsList.Add(new Goods { ItemId = 1, ItemName = "Soup", Price = .65, UnitofMeasure = "tin", HasDiscount = true });
+            _goodsList.Add(new Goods { ItemId = 2, ItemName = "Bread", Price = .80, UnitofMeasure = "loaf", HasDiscount = false });
+            _goodsList.Add(new Goods { ItemId = 3, ItemName = "Milk", Price = 1.3, UnitofMeasure = "bottle", HasDiscount = false });
+            _goodsList.Add(new Goods { ItemId = 4, ItemName = "Apples", Price = 1, UnitofMeasure = "bag", HasDiscount = true });
 
 
             CalculateTotal();
             CalculateDiscount();
+
+            _finalReceipt.TotalAmount = _finalReceipt.SubTotalAmount - _finalReceipt.DiscountAmount;
+
+            Console.WriteLine("Final Amount: " + _finalReceipt.TotalAmount);
         }
 
         private static void CalculateTotal()
@@ -34,44 +39,70 @@ namespace PriceBasket
             Double SubTotal = 0;
             Double UnitPrice = 0;
 
-            foreach (String Item in lCartItems)
+            foreach (String Item in _cartItems)
             {
-                UnitPrice = lGoods.Where(x => x.ItemName.ToUpper() == Item.ToUpper()).FirstOrDefault().Price;
+                UnitPrice = _goodsList.Where(x => x.ItemName.ToUpper() == Item.ToUpper()).FirstOrDefault().Price;
                 SubTotal = SubTotal + UnitPrice;
             }
 
-            FinalReceipt.SubTotalAmount = SubTotal;
-
+            _finalReceipt.SubTotalAmount = SubTotal;
         }
 
         private static void CalculateDiscount()
         {
-            int CountApples = lCartItems.Where(x => x.ToString().ToUpper() == "APPLES").Count();
-            int ApplesDiscount = 0;
-            if (CountApples > 0)
+            int countApples = _cartItems.Where(x => x.ToString().ToUpper() == "APPLES").Count();
+            Double discountApples = 0;
+
+            if (countApples > 0)
             {
+                discountApples = countApples * 0.10 *
+                                 _goodsList.Where(x => x.ItemName.ToUpper() == "APPLES").FirstOrDefault().Price;
 
             }
 
-            int CountSoup = lCartItems.Where(x => x.ToString().ToUpper() == "SOUP").Count();
+            _finalReceipt.DiscountAmount = _finalReceipt.DiscountAmount + discountApples;
+
+
+            int countSoup = _cartItems.Where(x => x.ToString().ToUpper() == "SOUP").Count();
+            int countBread = _cartItems.Where(x => x.ToString().ToUpper() == "BREAD").Count();
+            double discountBread = 0;
+
+            if ((countSoup > 1) && (countBread > 0))
+            {
+                int noofSoupForDiscount = countSoup / 2;
+
+                if (countBread <= noofSoupForDiscount)
+                {
+                    discountBread = countBread * 0.50 *
+                                    _goodsList.Where(x => x.ItemName.ToUpper() == "BREAD").FirstOrDefault().Price;
+
+                }
+
+            }
+
+            _finalReceipt.DiscountAmount = _finalReceipt.DiscountAmount + discountBread;
+
+
         }
 
-        private static void FormatDisplay()
+        private static void FormatOutputForDisplay()
         { }
     }
 
     class Goods
     {
+        public int ItemId { get; set; }
         public String ItemName { get; set; }
         public Double Price { get; set; }
         public String UnitofMeasure { get; set; }
+        public bool HasDiscount { get; set; }
     }
 
     class Receipt
     {
         public Double SubTotalAmount { get; set; }
         public String DiscountText { get; set; }
-        public String DiscountAmount { get; set; }
+        public Double DiscountAmount { get; set; }
         public Double TotalAmount { get; set; }
     }
 }
